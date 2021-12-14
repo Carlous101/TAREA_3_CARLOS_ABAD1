2a2969de866afc7362f6253490bdb6be91ef4fa9
Carlos Abad <67350301+Carlous101@users.noreply.github.com>
2021-12-14 15:27:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tarea_3_carlos_abad/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tarea_3_carlos_abad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
